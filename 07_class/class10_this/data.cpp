6a74dcf2de8db3b074866ee9a270b08422dbf063
jeff-cf <jianfuwave@gmail.com>
2024-05-03 23:24:03 +0800

    this指針

---
+++ 07_class/class10_this/data.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Data
+{
+private:
+    /* data */
+public:
+    int m_num = 50;
+    void setNum(int num)
+    {
+        m_num = num;
+    };
+    
+    int getNum() const
+    {
+        return m_num;
+    };
+
+};
+
+int main()
+{
+    Data ob1;
+    ob1.setNum(10);
+    cout << "ob1.m_num=" << ob1.m_num << endl;
+
+    Data ob2;
+    ob2.setNum(20);
+    cout << "ob2.m_num=" << ob2.m_num << endl;
+
+    Data ob3;
+    ob3.setNum(30);
+    cout << "ob3.m_num=" << ob3.m_num << endl;
+
+    const Data ob4;
+    //常對象只能調用const修飾的函數
+    cout << ob4.getNum() << endl;
+
+
+}
+
