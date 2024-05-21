c40a82962f1143341f4aec783f8a800dd7aa29ce
jeff-cf <jianfuwave@gmail.com>
2024-05-21 23:22:14 +0800

    繼承子類和父類同名靜態成員

---
+++ 08_class繼承/5_繼承子類和父類同名靜態成員/example.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+
+
+class Base
+{
+public:
+    //靜態成員
+    static int num; 
+    static int data; 
+    Base(){};
+    ~Base(){};
+    static void showData(){
+        cout << "Base的data=" << data << endl;
+    }
+};
+
+int Base::num = 100;
+int Base::data = 200;
+
+
+class Son:public Base
+{
+public:
+    static int data;
+    Son(){};
+    ~Son(){};
+    static void showData(){
+        cout << "Son的data=" << data << endl;
+    }
+};
+
+int Son::data = 200;
+
+
+
+
+
+int main(){
+    Son son;
+    Son::num = 10;
+    cout << Son::num << endl;
+    cout << Base::num << endl;
+
+    Son::data = 2;
+    cout << Son::data << endl;
+    //訪問父類的靜態成員
+    cout << Base::data << endl;
+    cout << son.Base::data << endl;
+    cout << Son::Base::data << endl;
+
+    //訪問父類和子類的同名靜態函數
+    Son::showData();
+    Son::Base::showData();
+}
